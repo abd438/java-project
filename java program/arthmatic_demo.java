cd72184a738810dc2a5504a492c0a78919150885
abd438 <demo63950@gmail.com>
2025-04-08 15:19:35 +0530

    Add files via upload

---
+++ java program/arthmatic_demo.java
@@ -0,0 +1,21 @@
+class arthmatic_demo
+{
+	public static void main(String args[])
+	{
+
+		int x=10, y=20;
+		int sum;
+		int sub;
+		int div;
+		int mul;
+		sum=x+y;
+		sub=x-y;
+		div=x/y;
+		mul=x*y;
+		System.out.println("addition of 2nos="+sum);
+		System.out.println("substration of 2nos="+sub);
+		System.out.println("division of 2nos="+div);
+		System.out.println("multyplication of 2nos="+mul);
+	}
+}
+	
